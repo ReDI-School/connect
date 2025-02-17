9439f6c93ed7e060ecfb02630d913534b589df4c
Eric Bolikowski <eric@binarylights.com>
2025-02-17 09:34:13 +0100

    Update the link for Malmo mentors (#1007) (#1008)
    Co-authored-by: Kateryna Voronina <51786805+katamatata@users.noreply.github.com>

--- apps/redi-connect/src/pages/app/me/onboarding-steps-config.tsx
+++ apps/redi-connect/src/pages/app/me/onboarding-steps-config.tsx
@@ -61,7 +61,7 @@ export const ONBOARDING_STEPS = [
           <a
             href={
               rediLocation === RediLocation.Malmo
-                ? 'https://calendar.app.google/zQJr8PJsNF2arm236'
+                ? 'https://calendar.app.google/u7EEPxtDVqif32Gz7'
                 : 'https://calendar.app.google/U1q1hfhfC3qdkkpq9'
             }
             target="__blank"
