303a0619ebd8cd3ae96478d5f88ae22895beac31
katamatata <kathrin.voronina@gmail.com>
2025-10-06 20:45:56 +0200

    Add chip variants

--- libs/shared-atomic-design-components/src/lib/atoms/Chip.tsx
+++ libs/shared-atomic-design-components/src/lib/atoms/Chip.tsx
@@ -1,10 +1,14 @@
+import classnames from 'classnames'
 import './Chip.scss'
 
+export type ChipVariant = 'default' | 'pending' | 'neutral'
 export interface ChipProps {
   chip: string
+  variant?: ChipVariant
 }
 
-const Chip = ({ chip }: ChipProps) => {
-  return <p className="chip">{chip}</p>
+const Chip = ({ chip, variant = 'default' }: ChipProps) => {
+  return <p className={classnames('chip', `chip--${variant}`)}>{chip}</p>
 }
+
 export default Chip
