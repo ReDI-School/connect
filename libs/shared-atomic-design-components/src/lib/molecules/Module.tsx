cf38cc968084f493224df0b17eda1c51585dd97d
katamatata <kathrin.voronina@gmail.com>
2026-01-05 15:04:13 +0100

    Add mentoring notes field, wip

--- libs/shared-atomic-design-components/src/lib/molecules/Module.tsx
+++ libs/shared-atomic-design-components/src/lib/molecules/Module.tsx
@@ -6,18 +6,22 @@ import './Module.scss'
 interface Props {
   title: string
   children: React.ReactNode
+  toggles?: React.ReactNode
   buttons?: React.ReactNode
   className?: string
 }
 
 function Module(props: Props) {
-  const { title, children, buttons, className } = props
+  const { title, children, toggles, buttons, className } = props
 
   return (
     <div className={classnames('module', { [`${className}`]: className })}>
       <div className="module__header">
         <Caption>{title}</Caption>
-        <div className="module__header__buttons"> {buttons && buttons}</div>
+        <div className="module__header__buttons">
+          <div title="Toggle meeting notes"> {toggles && toggles}</div>
+          <div title="Add new session"> {buttons && buttons}</div>
+        </div>
       </div>
       <div className="module__body">{children}</div>
     </div>
